8b3572985eb252998ab6b74adb07ef4d7bb4f374
Yahn <yahnnyhay123@gmail.com>
2023-09-25 11:54:09 -0300

    escrever função para limpar formulario

---
+++ controller.js
@@ -0,0 +1,10 @@
+'use restrict'; //modo restrito
+
+//limpar formulario
+const limparFormulario = (endereco) =>{
+    document.getElementById('rua').value = '';
+    document.getElementById('bairro').value = '';
+    document.getElementById('cidade').value = '';
+    document.getElementById('estado').value = '';
+
+}
